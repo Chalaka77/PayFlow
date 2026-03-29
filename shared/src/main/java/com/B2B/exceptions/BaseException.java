31d860f89a169a7c469276d0bb4f1ff302926b77
Chalaka77 <baccoalberto@yahoo.it>
2026-03-29 12:29:12 +0200

    BaseException + ErrorResponse

---
+++ shared/src/main/java/com/B2B/exceptions/BaseException.java
@@ -0,0 +1,19 @@
+package com.B2B.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public class BaseException extends RuntimeException
+{
+    private final HttpStatus status;
+
+    public BaseException(String message, HttpStatus status)
+    {
+        super(message);
+        this.status = status;
+    }
+
+    public HttpStatus getStatus()
+    {
+        return status;
+    }
+}
