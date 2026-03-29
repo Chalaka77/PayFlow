9bc180bdc5fb72ea90edb529959abcd1f2fe10cd
Chalaka77 <baccoalberto@yahoo.it>
2026-03-29 10:45:42 +0200

    first push

---
+++ src/main/java/com/b2b/payflow/PayFlowApplication.java
@@ -0,0 +1,15 @@
+package com.b2b.payflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayFlowApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(PayFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/b2b/payflow/PayFlowApplicationTests.java
@@ -0,0 +1,15 @@
+package com.b2b.payflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayFlowApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
